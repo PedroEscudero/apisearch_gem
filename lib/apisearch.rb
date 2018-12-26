82c17fb3f39167bfa77618678da0eb7e67f3a7d9
PedroEscudero <pedroescudero.zumel@gmail.com>
2018-12-26 19:31:32 +0100

    Fix raise control

--- lib/apisearch.rb
+++ lib/apisearch.rb
@@ -9,21 +9,17 @@ class Apisearch
   @token = ''
 
   def initialize(params = {})
-    params.url.value  ? @url = params.url.value : raise 'No url'
-    params.url.app_id ? @app_id = params.app_id.value : raise 'No app id'
-    params.url.token  ? @token = params.token.value : raise 'No valid token'
+    params.url.value  ? @url = params.url.value : raise ('No url')
+    params.url.app_id ? @app_id = params.app_id.value : raise ('No app id')
+    params.url.token  ? @token = params.token.value : raise ('No valid token')
   end
 
   def search
-<<<<<<< HEAD
     if @query
       Net::HTTP.get('https://apisearch.global.ssl.fastly.net', '/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={hel}')
     else
       Net::HTTP.get('https://apisearch.global.ssl.fastly.net', '/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={#{@query}}')
     end
-=======
-
->>>>>>> 1b4a5b83af50840c1a6a6195d225fd87cdf4677e
   end
 
   def set_query(query)

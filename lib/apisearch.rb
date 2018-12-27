f13a6774fa4ef73cadc21a58c1447297ce797607
PedroEscudero <pedroescudero.zumel@gmail.com>
2018-12-27 12:33:15 +0100

    Change http library

--- lib/apisearch.rb
+++ lib/apisearch.rb
@@ -1,5 +1,6 @@
 require 'net/http'
-
+require "open-uri"
+@data =
 class Apisearch
 
   @url = ''
@@ -7,16 +8,16 @@ class Apisearch
   @token = ''
 
   def initialize(params = {})
-    @url = params[:url]
-    @app_id = params[:app_id]
+    @url = params[:url] || ''
+    @app_id = params[:app_id] || ''
   end
 
   def search
     if !@query
-      puts 'http://apisearch.global.ssl.fastly.net/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={hel}'
-      Net::HTTP.get('http://apisearch.global.ssl.fastly.net', '/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={"q":"hel"}')
+      puts 'http://apisearch.global.ssl.fastly.net/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={"q":"hel"}'
+      URI.parse('http://apisearch.global.ssl.fastly.net/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={"q":"hel"}').read
     else
-      Net::HTTP.get('http://apisearch.global.ssl.fastly.net', '/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={"q":"#{@query}"}')
+      #Net::HTTP.get('http://apisearch.global.ssl.fastly.net', '/v1?app_id=54725861&token=daf93c2b-40bc-49f2-870e-f8f62ea524ad&query={"q":"#{@query}"}')
     end
   end
 
